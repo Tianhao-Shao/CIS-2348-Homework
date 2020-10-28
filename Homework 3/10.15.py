8a84ec4dc51d411b41377e5a212da03da1800fb1
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2020-10-28 12:22:26 -0500

    Update 10.15.py

--- Homework 3/10.15.py
+++ Homework 3/10.15.py
@@ -15,14 +15,10 @@ class Team:
 if __name__ == "__main__":
 	
 	team = Team()
-   
-	team_name = input()
-	team_wins = int(input())
-	team_losses = int(input())
 	
-	team.team_name = team_name
-	team.team_wins = team_wins
-	team.team_losses = team_losses
+	team.team_name = input()
+	team.team_wins = int(input())
+	team.team_losses = int(input())
 	
 	if team.get_win_percentage() >= 0.5:
 		print('Congratulations, Team', team.team_name,'has a winning average!')
